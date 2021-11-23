3746445edd94cdbee8010abcdc1a12d9a5199322
Tatenda <93754415+Tatenda-NS@users.noreply.github.com>
2021-11-23 04:59:49 -0800

    TwoSum using HasMap

---
+++ src/TwoSumHashMap.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class TwoSumHashMap {
+    public static int[] sum(int[] nums, int target) {
+        if (nums.length < 1)
+            return nums;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++)
+        {
+            if (map.containsKey(target - nums[i]))
+            {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            else
+            {
+                map.put(nums[i], i);
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 2, 5, 6, 7, 8};
+        int target = 11;
+        System.out.println(Arrays.toString(sum(nums, target)));
+    }
+}
