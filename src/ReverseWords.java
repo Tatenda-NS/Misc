6c6311918a8aa4bb6feff7a51d0670438952677b
Tatenda <93754415+Tatenda-NS@users.noreply.github.com>
2021-11-23 06:00:27 -0800

    Reverse a sentence word by word

---
+++ src/ReverseWords.java
@@ -0,0 +1,17 @@
+public class ReverseWords {
+    public static String reverse(String sentence) {
+        StringBuilder solution = new StringBuilder();
+        String[] sentenceArray = sentence.split(" ");
+
+        for (int i = sentenceArray.length - 1; i >= 0; i--)
+        {
+            solution.append(" ").append(sentenceArray[i]);
+        }
+        return solution.toString();
+    }
+
+    public static void main(String[] args) {
+        String sentence = "Good morning and how are you";
+        System.out.println(reverse(sentence));
+    }
+}
